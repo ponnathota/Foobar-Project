7b95dbdb27186e9cb82d0f6100905b3907f606f9
venkata krishna ponnathota <raghava123reddy@gmail.com>
2016-07-15 16:23:31 -0500

    my changes

--- Foobar_Code.R
+++ Foobar_Code.R
@@ -1,19 +1,44 @@
 #Loading Libraries#
 install.packages("Hmisc")
-library(Hmisc)
 install.packages("data.table")
+install.packages("dplyr")
+
+library(Hmisc)
 library(data.table)
-install.packages(dplyr)
 library(dplyr)
 
+
 #Working Directories
-setwd("C:/Users/1320128/Desktop/Foobar-Project")
+setwd(choose.dir(getwd(), "set working directory"))
 #Loading Data
-foobar_v1 <- fread(input ="widgets.csv", sep="auto",
-                   showProgress = getOption("datatable.showProgress"))
+foobar_v1 <- fread(
+  input = "widgets.csv",
+  sep = "auto",
+  showProgress = getOption("datatable.showProgress")
+)
+attach(foobar_v1)
 str(foobar_v1)
 describe(foobar_v1)
 
-foobar_v2<-na.omit(foobar_v1)
-?dplyr
+dataNoNA <- foobar_v1[!is.na(style),]
+attach(dataNoNA)
+dataNonNegPrice <- dataNoNA[price > 0,]
+attach(dataNonNegPrice)
+descriptiveStats <- describe(dataNonNegPrice)
+descriptiveStats
+hist(price)
 
+foobar_v2 <- na.omit(foobar_v1)
+?dplyr
+foobar_v1
+foobar_v1
+barplot(
+  table(foobar_v1$style, useNA = "no"),
+  xlab = "style",
+  ylab = "frequency",
+  axes = TRUE,
+  axisnames = TRUE,
+  srt = 45
+)
+?barplot
+?table
